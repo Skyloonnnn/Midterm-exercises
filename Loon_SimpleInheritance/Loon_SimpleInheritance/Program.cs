636cc7549a42083d24fc084eced1ccf7082e1c21
ACT-STUDENT <ACT-STUDENT@LAB3-PC27>
2025-09-01 14:51:28 +0800

    Exercise 6

---
+++ Loon_SimpleInheritance/Loon_SimpleInheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_SimpleInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sky Loon
+            //IT306
+            //Exercise 6: Simple Inheritance
+
+            //Instantiate the RegularStudent class
+            RegularStudent regularStudent = new RegularStudent
+            {
+                Name = "Sky Loon",
+                Program = "Bachelor of science in information technology",
+                Section = "IT306"
+                
+            };
+            regularStudent.BasicInfo(); // Call the method to display Student class
+            regularStudent.SectionEnrolled(); // call the method to display RegularStudent class
+
+            //Instantiate the IrregularStudent class
+            IrregularStudent irregularStudent = new IrregularStudent
+            {
+                Name = "Aian Cuyos",
+                Program = "Bachelor of science in information technology",
+                UnitsEnrolled = 18
+            };
+            irregularStudent.BasicInfo(); // Call the method to display Student class
+            irregularStudent.EnrolledSemUnits(); //  Call the method to display IrregularStudent class
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Loon_SimpleInheritance/Loon_SimpleInheritance/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loon_SimpleInheritance
+{
+    class Student //Base class
+    {
+        //Properties for the student class
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        //Method to display student information
+        public void BasicInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n\tStudent Information");
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine($"Name: {Name} \nProgram: {Program}");
+        }
+
+    }
+    class RegularStudent : Student //Derived class
+    {
+        //Properties for the RegularStudent class
+        public string Section { get; set; }
+
+        //Method to display RegularStudent class
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"Section Enrolled: {Section}");
+        }
+    }
+    class IrregularStudent : Student //Derives class
+    {
+        //Properties for the IrregularStudent class
+        public int UnitsEnrolled { get; set; }
+
+        //method to display IrregularStudent class
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine($"Units enrolled: {UnitsEnrolled}");
+        }
+
+    }
+  
+}
